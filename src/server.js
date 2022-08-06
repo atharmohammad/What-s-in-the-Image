2b7826b7a8e54e8ea9342712850fea386dbd3952
Ritik Choudhary <ritikchoudharydd@gmail.com>
2022-08-07 00:33:12 +0530

    Drag and Drop on client side

--- src/server.js
+++ src/server.js
@@ -1,67 +1,62 @@
-const express = require('express')
-const cors = require('cors')
-const { spawn } = require('child_process')
-const chalk = require('chalk')
+const express = require("express")
+const cors = require("cors")
+const { spawn } = require('child_process');
+const chalk = require("chalk");
 const multer = require('multer')
-const delay = () =>
-  new Promise((resolve) => {
+const delay = () => new Promise(resolve => {
     setTimeout(() => {
-      resolve()
-    }, 10000)
-  })
+        resolve();
+    }, 10000);
+});
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'src/Images/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '.jpg') //Appending .jpg
-  },
+    destination: function (req, file, cb) {
+      cb(null, 'src/Images/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '.jpg') //Appending .jpg
+    }
 })
 
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage });
 
-const createServer = () => {
-  const app = express()
+const createServer = ()=>{
+    const app = express();
 
-  app.use(
-    cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 200,
+    app.use(
+        cors({
+            "origin": '*',
+            "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+            "preflightContinue": false,
+            "optionsSuccessStatus": 200
+        })
+      );
+      
+    app.use(express.json());
+
+    app.get('/',(req,res,next)=>{
+        return res.status(200).send("Index Page")
     })
-  )
 
-  app.use(express.json())
+    app.post('/api/predict',upload.single('file'),async(req,res)=>{
+        try{
+            const file = req.file.filename
+            const pyprog = spawn('python', [__dirname + '/predict.py',file]);
+            pyprog.stderr.pipe(process.stderr);
+            let ans = 'Work';
+            await pyprog.stdout.on('data', function(data) {
+                console.log(chalk.red('-------------------------------------------------------------------------'));
+                console.log(chalk.blue('Prediction About Image is :-'));
+                console.log(chalk.green(data));
+                ans = data
+            });
+            await delay()
+            res.send(ans)
+        }catch(e){
+            res.send(e)
+        }
+    })
 
-  app.get('/', (req, res, next) => {
-    return res.status(200).send('Index Page')
-  })
-
-  app.post('/api/predict', upload.single('file'), async (req, res) => {
-    try {
-      const file = req.file.filename
-      const pyprog = spawn('python', [__dirname + '/predict.py', file])
-      pyprog.stderr.pipe(process.stderr)
-      let ans = 'Work'
-      await pyprog.stdout.on('data', function (data) {
-        console.log(
-          chalk.red(
-            '-------------------------------------------------------------------------'
-          )
-        )
-        console.log(chalk.blue('Prediction About Image is :-'))
-        console.log(chalk.green(data))
-        ans = data
-      })
-      await delay()
-      res.send(ans)
-    } catch (e) {
-      res.send(e)
-    }
-  })
-
-  return app
+    return app;
 }
 
 module.exports = createServer
